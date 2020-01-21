6ee71d58dc5602e6c4bd69114fe775b443b6eee8
Henrico <henrico.almeida@fcamara.com.br>
2020-01-21 16:10:52 -0300

    Initial Commit

---
+++ buscador_de_gifs/android/app/src/main/kotlin/br/com/fcamara/buscador_de_gifs/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fcamara.buscador_de_gifs
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
